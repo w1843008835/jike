8d525d16fdd5caf433b17251fe690a401ffc562b
w1843008835 <chenyw1843@gmail.com>
2024-03-27 22:43:29 +0900

    表单校验

---
+++ src/pages/Login/index.js
@@ -0,0 +1,41 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+
+import logo from '@/assets/logo.png'
+const Login = () => {
+    return <div className='login'>
+        <Card className='=login-container'>
+            <img className='login-logo' src={logo} alt=''></img>
+            <Form validateTrigger='onBlur'>
+                <Form.Item
+                    name="username"
+                    rules={[{
+                        required: true,
+                        message: 'input your username'
+                    },
+                    {
+                        pattern: /^1[3-9]\d{9}/,
+                        message: '请输入正确的手机号'
+                    }
+                    ]}>
+                    <Input size="large" placeholder="phone number"></Input>
+                </Form.Item>
+                <Form.Item name="password"
+                    rules={[{
+                        required: true,
+                        message: 'input your password'
+                    },
+                    ]}>
+                    <Input size="large" placeholder="password"></Input>
+                </Form.Item>
+                <Form.Item>
+                    <Button size="primary" htmlType="submit" block>登录</Button>
+                </Form.Item>
+            </Form>
+
+        </Card>
+
+    </div >
+}
+
+export default Login
