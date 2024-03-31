9d0c7b0fde5da2d830c1ff049aba33e6708a2e3b
w1843008835 <chenyw1843@gmail.com>
2024-03-31 23:34:20 +0900

    echarts基础图表渲染

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,36 @@
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react';
+
+
 const Home = () => {
-    return <div>Home</div>
+    const chartRef = useRef(null)
+    //保证dom可用再进行渲染
+    useEffect(() => {
+        const chartDom = chartRef.current;
+        const myChart = echarts.init(chartDom);
+
+
+        const option = {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [120, 200, 150, 80, 70, 110, 130],
+                    type: 'bar'
+                }
+            ]
+        };
+
+        option && myChart.setOption(option);
+    }, [])
+    return <div>
+
+        <div ref={chartRef} style={{ width: '500px', height: '400px' }}></div>
+    </div>
 }
 export default Home
