71c96dadc3a93c3e745e7bf01fd7d8da544ba2b7
w1843008835 <chenyw1843@gmail.com>
2024-03-31 11:35:12 +0900

    二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,22 @@
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login"
 import About from "@/pages/About";
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRoute } from "@/components/AuthRoute";
 const router = createBrowserRouter([
     //{ path: '/', element: <AuthRoute><Layout /></AuthRoute> }, 没有token跳转到登录画面
-    { path: '/', element: <Layout /> },
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            { index: true, element: <Home /> },
+            { path: 'article', element: <Article /> },
+            { path: 'publish', element: <Publish /> },
+        ]
+    },
     { path: '/login', element: <Login /> },
     { path: '/about', element: <About /> },
 ])
